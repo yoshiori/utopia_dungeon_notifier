fc390a16f253168cd8beeddb54705308ea9430d9
yoshiori <yoshiori@gmail.com>
2014-02-27 22:50:15 +0900

    Remove pit

--- lib/utopia_dungeon_notifier/cli.rb
+++ lib/utopia_dungeon_notifier/cli.rb
@@ -2,21 +2,24 @@ require "utopia_dungeon_notifier"
 
 require "summons_board"
 require 'thor'
-require "pit"
 require "im-kayac"
 
 module UtopiaDungeonNotifier
   class CLI < Thor
-    desc "check \${uid}", "check event."
-    def check(uid)
+    desc "check \${uid} \${imKayac_UserName} \${imKayac_SecretKey}", "check event."
+    def check(uid, user_name, secret_key)
+      @user_name = user_name
+      @secret_key = secret_key
       event = SummonsBoard::Event.new(uid: uid)
       if event.active?
         im_kyac.post('ユートピアダンジョンスタート')
       end
     end
 
-    desc "schedule \${uid}", "send event schedule."
-    def schedule(uid)
+    desc "schedule \${uid} \${imKayac_UserName} \${imKayac_SecretKey}", "send event schedule."
+    def schedule(uid, user_name, secret_key)
+      @user_name = user_name
+      @secret_key = secret_key
       event = SummonsBoard::Event.new(uid: uid)
       text = "今日のユートピアダンジョンは "
       event.schedule[:times].each do |active_time|
@@ -27,11 +30,7 @@ module UtopiaDungeonNotifier
 
     private
     def im_kyac
-      @im_kayac ||= ImKayac.to(pit["username"]).password(pit["password"])
-    end
-
-    def pit
-      @pit ||= Pit.get('im.kayac.com')
+      @im_kayac ||= ImKayac.to(@user_name).secret(@secret_key)
     end
   end
 end
--- utopia_dungeon_notifier.gemspec
+++ utopia_dungeon_notifier.gemspec
@@ -19,7 +19,6 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "thor"
-  spec.add_dependency "pit"
   spec.add_dependency "im-kayac"
 
   spec.add_development_dependency "bundler", "~> 1.3"
